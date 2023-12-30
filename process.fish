aa5d8f81d170c5b612d90a088c5aa5cd661a7bea
xzxz <46389288+J-XZ@users.noreply.github.com>
2023-12-30 14:10:59 +0800

    1

--- process.fish
+++ process.fish
@@ -10,13 +10,15 @@ set output_file2 (dirname $input_file)/(basename $input_file .pdf)_margin_compre
 python3.11 $current_path/pdfm.py $input_file $output_file1
 python3.11 $current_path/pdfc.py -c 4 -o $output_file2 $output_file1
 
+rm $output_file1
+
 # 比较文件大小，并删除较大的一个
-set size1 (stat -f %z $output_file1)
-set size2 (stat -f %z $output_file2)
-if test $size1 -gt $size2
-    rm $output_file1
-else
-    rm $output_file2
-    mv $output_file1 $output_file2
-end
+# set size1 (stat -f %z $output_file1)
+# set size2 (stat -f %z $output_file2)
+# if test $size1 -gt $size2
+    # rm $output_file1
+# else
+    # rm $output_file2
+    # mv $output_file1 $output_file2
+# end
 
